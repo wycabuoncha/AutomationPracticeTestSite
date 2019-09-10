59acd3f86c76c311a21df92f82e14501c4c72678
OnchariWycliffe <onchariwycliffe.qa@gmail.com>
2019-09-10 17:33:58 +0300

    started framework 2

---
+++ src/main/java/selenium/Core/ChromeDriverManager.java
@@ -0,0 +1,24 @@
+package selenium.Core;
+
+public class ChromeDriverManager extends DriverManager {
+
+	private ChromeDriverService chService;
+	@Override
+	protected void startService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void stopService() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void createDriver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
